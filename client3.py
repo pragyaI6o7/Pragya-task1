9a839ab71e4e6da1baa162f0caf7c95468984876
Pragya <ranip16july@gmail.com>
2023-06-06 17:32:17 +0530

    client3.py

--- client3.py
+++ client3.py
@@ -48,7 +48,7 @@ def getRatio(price_a, price_b):
 
 
 # Main
-if __name__ == "__main__":
+if __name__ == "__main__": 
     # Query the price once every N seconds.
     for _ in iter(range(N)):
         quotes = json.loads(urllib.request.urlopen(QUERY.format(random.random())).read())
